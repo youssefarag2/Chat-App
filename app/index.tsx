6d5b888ed0dd487ced89a7533d2f9ad1607e0be7
youssefarag2 <youssefarag19@gmail.com>
2025-10-28 18:59:03 +0300

    configuring styling

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-500 font-bold">Welcome</Text>
+      <Link href="/onboarding">Onbording</Link>
+    </View>
+  );
+}
